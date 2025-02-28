0918e437e775527290e859f6bc05683c3949eb23
Ronan Scotet <102806074+domov44@users.noreply.github.com>
2025-02-28 18:25:14 +0100

    package update

--- database/migrations/2025_02_28_133936_create_recipes_table.php
+++ database/migrations/2025_02_28_133936_create_recipes_table.php
@@ -9,22 +9,18 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('recipes', function (Blueprint $table) {
             $table->id();
-            $table->string(column: "title");
-            $table->text(column: "description")->nullable();
-            $table->integer(column:"duration")->nullable();
-            $table->json(column:"steps")->nullable();
+            $table->string('title');
+            $table->text('description');
+            $table->json('steps');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('recipes');
     }
